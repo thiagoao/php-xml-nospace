7e21dc74d69c505dc05e75909ee6eed48bd6a387
Thiago Augustus de Oliveira <thiagoao@users.noreply.github.com>
2016-03-18 15:29:00 -0300

    Update NoSpaceTest.php

--- Tests/NoSpaceTest.php
+++ Tests/NoSpaceTest.php
@@ -4,7 +4,7 @@ class NoSpaceTest extends PHPUnit_Framework_TestCase
 {
 
     public function testNoXMLSpace() {
-        $xml = simplexml_load_file( 'xml_file.xml' );
+        $xml = simplexml_load_file( 'xml_test.xml' );
         $xml = NoXMLSpace::noSpace($xml);
         
         $this->assertEquals('Hello World', $xml->message);
